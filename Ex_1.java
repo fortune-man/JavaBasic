a06c2ff370f9ed91ad902a0fafa3c6c98e17f013
pepperoni <79819812+yourtimeislimited@users.noreply.github.com>
2022-02-07 23:18:21 +0900

    Add files via upload

---
+++ Ex_1.java
@@ -0,0 +1,22 @@
+public class Ex_1 {
+    public static void main(String[] args) {
+        Tv t = new Tv();
+        t.channel = 7;
+        t.channelDown();
+        System.out.println("t.channel = " + t.channel);
+
+    }
+
+}
+
+class Tv {
+    String color;
+    boolean power;
+    int channel;
+
+    void power() {power = !power;}
+    void channelUp() {++channel;}
+    void channelDown() {--channel;}
+
+
+}
